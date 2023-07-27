b244518e4ebf1fd7165c5db9d550738311792720
DevikaRajesh22 <devikaraj699@gmail.com>
2023-07-27 13:56:50 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function btnClick(){
-    alert("Ordered");
+const model=document.querySelector('.model');
+const orderBtn=document.querySelector('.loginBtn');
+const closeBtn=document.querySelector('.close');
+
+orderBtn.addEventListener('click',openModel);
+closeBtn.addEventListener('click',closeModel);
+window.addEventListener('click',outsideClick);
+
+function openModel(){
+    model.style.display="block";
+}
+
+function closeModel(){
+    model.style.display='none';
+}
+
+function outsideClick(e){
+    if(e.target==model){
+        closeModel()
+    }
 }
